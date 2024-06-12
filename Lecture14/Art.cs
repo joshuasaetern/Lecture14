7738e88e044e36d29a2d311900163ce0c6fb58d1
joshuasaetern <JoshuaSaetern17@gmail.com>
2024-06-11 23:58:17 -0700

    Add project files.

---
+++ Lecture14/Art.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture14
+{
+    internal class Art
+    {
+        //Enumerables
+        //Keyword: enum
+
+        public enum STYLES { Abstract, Impressionism, Cubism }
+
+
+        //Fields
+        STYLES style;
+        String name;
+
+        //Constructor
+        public Art(string name, STYLES style)
+        {
+            this.name = name;
+            this.style = style;
+        }
+
+        //Properties
+        public STYLES Style { get => style; set => style = value; }
+        public string Name { get => name; set => name = value; }
+
+        //Methods
+        public override string ToString()
+        {
+            return $"{this.name} - {this.style}";
+        }
+    }
+}
---
+++ Lecture14/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Win32;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lecture14
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            FillComboBox();
+
+            imgDisplay.Source = ConvertToImage("C:\\Users\\tehso\\source\\repos\\Lecture14\\Lecture14\\Images\\cheetah.jpg");
+        }
+        public void FillComboBox()
+        {
+            Art nighthawks = new Art("Nighthawks", Art.STYLES.Impressionism);
+
+            runDisplay.Text = nighthawks.ToString();
+
+            cmbStyles.ItemsSource = Enum.GetValues(typeof(Art.STYLES)).Cast<Art.STYLES>().ToList();
+        }
+        public BitmapImage ConvertToImage(String filePath)
+        {
+            String imgPath = @filePath;
+
+            //Uniform Resource Identifier
+            Uri convertPath = new Uri(imgPath);
+            BitmapImage bitmap = new BitmapImage(convertPath);
+
+            return bitmap;
+        }
+        private void btnLoadImage_Click(object sender, RoutedEventArgs e)
+        {
+            //Step 1
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            //Filter
+            //What is displayed in drop down box
+            String displayFilter = "Image files (*.png;*.jpeg;*.jpg;)";
+            //Filter results in file explorer
+            String filterBy = "*.png;*.jpeg;*.jpg;";
+            //Full string which uses pipe to seperate display and filter
+            String fullFilter = $"{displayFilter}|{filterBy}";
+
+            openFileDialog.Filter = fullFilter;
+            
+            //Opens dialog and returns true if image is selected
+            if (openFileDialog.ShowDialog() == true)
+            {
+                String returnedFiledPath = openFileDialog.FileName;
+                imgDisplay.Source = ConvertToImage(returnedFiledPath);
+            }
+        }
+    }
+}
